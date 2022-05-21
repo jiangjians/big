1c0e195b4e4dcb77b6547a84dc3600d87cfa3bb3
jiangjians <1455928402@qq.com>
2022-05-21 10:14:53 +0800

    完成首页

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,17 @@ function getUserInfo() {
       if (res.status !== 0) return layer.msg("数据请求失败！");
       layer.msg("数据请求成功！");
       renderAvatar(res.data)
-    }
+    },
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    // complete: (res) => {
+    //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    //   if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+    //     //  强制清空 token
+    //     localStorage.removeItem("token");
+    //     // 强制跳转到登录页面
+    //     location.href = "/login.html"
+    //   }
+    // }
   })
 }
 // 渲染用户头像
@@ -35,3 +45,16 @@ const renderAvatar = (user) => {
     $('.text-avatar').html(firstName).show()
   }
 }
+
+// 退出登录
+$("#btnLogout").click(() => {
+  layui.layer.confirm(
+    "确定退出登录？", { icon: 3, title: "" },
+    function(index) {
+      // 清空本地存储里面的 token
+      localStorage.removeItem("token");
+      // 重新跳转到登录页面
+      location.href = "/login.html";
+    }
+  );
+});
